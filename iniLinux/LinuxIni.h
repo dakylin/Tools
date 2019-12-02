6fc2f0545e4c51a594677248a51b6fbfc80c744b
Daniel Hu <danielhu83@outlook.com>
2019-12-02 08:54:38 +0800

    Check key before get value

--- iniLinux/LinuxIni.h
+++ iniLinux/LinuxIni.h
@@ -3,39 +3,41 @@
 #include <map>
 #include <string>
 using namespace std;
- 
-#define CONFIGLEN           256 
- 
+
+#define CONFIGLEN 256
+
 enum INI_RES
 {
-    INI_SUCCESS,  
-    INI_ERROR,   
-    INI_OPENFILE_ERROR, 
-    INI_NO_ATTR 
+	INI_SUCCESS,
+	INI_ERROR,
+	INI_OPENFILE_ERROR,
+	INI_NO_SECTION,
+	INI_NO_ENTRY
 };
- 
 
-typedef map<std::string,std::string> KEYMAP;
-  
-typedef map<std::string,KEYMAP> MAINKEYMAP;
+typedef map<std::string, std::string> KEYMAP;
 
- 
-class LinuxIni 
+typedef map<std::string, KEYMAP> MAINKEYMAP;
+
+class LinuxIni
 {
 public:
-    LinuxIni();
-    virtual ~LinuxIni();
+	LinuxIni();
+	virtual ~LinuxIni();
+
 public:
-    bool  GetInt(const char* mAttr, const char* cAttr, int &nRes );
-    bool GetStr(const char* mAttr, const char* cAttr, char *cValue, size_t nSize );
-    INI_RES OpenFile(const char* pathName, const char* type);
-    INI_RES CloseFile();
+	bool GetInt(const char *mAttr, const char *cAttr, int &nRes);
+	bool GetStr(const char *mAttr, const char *cAttr, char *cValue, size_t nSize);
+	INI_RES OpenFile(const char *pathName, const char *type);
+	INI_RES CloseFile();
+
 protected:
-    INI_RES GetKey(const char* mAttr, const char* cAttr, char* value);
+	INI_RES GetKey(const char *mAttr, const char *cAttr, char *value);
+
 protected:
-    FILE* m_fp;
-    char  m_szKey[ CONFIGLEN ];
-    MAINKEYMAP m_Map;
+	FILE *m_fp;
+	char m_szKey[CONFIGLEN];
+	MAINKEYMAP m_Map;
 };
 
 #endif
